7f53137ad942de5f11c1c819fd3a5810773649d5
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-06-05 15:31:06 +0100

    test: retrieve empty list if user is not a friend

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,18 @@
-﻿namespace TripServiceKata.Tests
-{
-    public class TripServiceShould
-    {
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using TripServiceKata.Entity;
+
+namespace TripServiceKata.Tests {
+    public class TripServiceShould {
+        [Test]
+        public void RetrieveEmptyListIfUserIsNotAFriend() {
+            TripService tripService = new TripService();
+            var user = new User();
+
+            List<Trip> tripList = tripService.GetTripsByUser(user);
+
+            tripList.Should().BeEmpty();
+        }
     }
 }
