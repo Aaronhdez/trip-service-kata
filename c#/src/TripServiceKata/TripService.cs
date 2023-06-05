0815f1002635a468ac8a5e87451f02ccb243f6ef
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-06-05 16:10:37 +0100

    refactor: if inverted

--- c#/src/TripServiceKata/TripService.cs
+++ c#/src/TripServiceKata/TripService.cs
@@ -4,10 +4,8 @@ using TripServiceKata.Entity;
 using TripServiceKata.Exception;
 using TripServiceKata.Service;
 
-namespace TripServiceKata
-{
-    public class TripService
-    {
+namespace TripServiceKata {
+    public class TripService {
         private readonly IUserSession _userSession;
         private ITripDAO _tripDao;
 
@@ -16,21 +14,15 @@ namespace TripServiceKata
             _tripDao = tripDao;
         }
 
-        public List<Trip> GetTripsByUser(User user)
-        {
+        public List<Trip> GetTripsByUser(User user) {
             var tripList = new List<Trip>();
             var loggedUser = _userSession.GetLoggedUser();
-            if (loggedUser != null)
-            {
-                if (Enumerable.Contains(user.GetFriends(), loggedUser)) {
-                    tripList = _tripDao.FindTripsByUser(user);
-                }
-
-
+            if (loggedUser != null) {
+                if (!Enumerable.Contains(user.GetFriends(), loggedUser)) return tripList;
+                tripList = _tripDao.FindTripsByUser(user);
                 return tripList;
             }
-            else
-            {
+            else {
                 throw new UserNotLoggedInException();
             }
         }
