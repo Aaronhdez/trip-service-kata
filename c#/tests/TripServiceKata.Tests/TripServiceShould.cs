a43bdb1dcd513161e39d6827e3294bb46ecace21
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-06-06 13:44:49 +0100

    test: a characterization test

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,19 @@
-﻿namespace TripServiceKata.Tests
-{
-    public class TripServiceShould
-    {
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TripServiceKata.Entity;
+using TripServiceKata.Exception;
+
+namespace TripServiceKata.Tests {
+    public class TripServiceShould {
+        [Test]
+        public void ACharacterizationTest() {
+            var tripService = new TripService();
+            var user = new User();
+
+            Action result = () => tripService.GetTripsByUser(user);
+
+            result.Should().Throw<UserNotLoggedInException>();
+        }
     }
 }
